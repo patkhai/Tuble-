091d028ead1784ca488a3bac4a81a120f0e3ccf9
Kam Khai <patrick.phyo@gmail.com>
2019-02-04 19:40:33 -0800

    Initial Commit

---
+++ tubleTests/tubleTests.swift
@@ -0,0 +1,36 @@
+//
+//  tubleTests.swift
+//  tubleTests
+//
+//  Created by Pat Khai on 2/4/19.
+//  Copyright © 2019 Pat Khai. All rights reserved.
+//
+
+import XCTest
+@testable import tuble
+
+class tubleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
